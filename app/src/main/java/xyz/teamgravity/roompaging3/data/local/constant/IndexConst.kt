625b4f09ab6c2cb859720c5c341b23c6e018c48d
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-11 17:23:57 +0500

    add constants for index database

---
+++ app/src/main/java/xyz/teamgravity/roompaging3/data/local/constant/IndexConst.kt
@@ -0,0 +1,21 @@
+package xyz.teamgravity.roompaging3.data.local.constant
+
+object IndexConst {
+
+    /**
+     * Database name
+     */
+    const val NAME = "database_index"
+
+    /**
+     * Database version
+     */
+    const val VERSION = 1
+
+    /**
+     * Table index
+     * Entity -> [xyz.teamgravity.roompaging3.data.local.entity.IndexEntity]
+     * Model -> [xyz.teamgravity.roompaging3.data.model.IndexModel]
+     */
+    const val TABLE_INDEX = "table_index"
+}
