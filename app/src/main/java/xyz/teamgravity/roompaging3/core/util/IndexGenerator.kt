b082fa655e2f1f7182f2e77d69032da4cd3117f5
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-11 17:31:34 +0500

    add index generator

--- app/src/main/java/xyz/teamgravity/roompaging3/core/util/IndexGenerator.kt
+++ app/src/main/java/xyz/teamgravity/roompaging3/core/util/IndexGenerator.kt
@@ -1,12 +1,16 @@
 package xyz.teamgravity.roompaging3.core.util
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import xyz.teamgravity.roompaging3.data.model.IndexModel
 
 class IndexGenerator {
 
-    fun generateIndexes(): List<IndexModel> {
-        val indexes = mutableListOf<IndexModel>()
-        repeat(600) { indexes.add(IndexModel(index = it.toLong())) }
-        return indexes
+    suspend fun generateIndexes(): List<IndexModel> {
+        return withContext(Dispatchers.Default) {
+            val indexes = mutableListOf<IndexModel>()
+            repeat(600) { indexes.add(IndexModel(index = it.toLong())) }
+            return@withContext indexes
+        }
     }
 }
